5b725ef7fdf346ee48d6acc4ab0cc56d16573e90
Nick Saunders <nick@saunde.rs>
2019-09-27 12:16:11 -0700

    List of greetings

--- example/Greeting.purs
+++ example/Greeting.purs
@@ -2,53 +2,63 @@ module Example.Greeting where
 
 import Prelude
 import Control.Monad.Except (ExceptT)
-import Control.Monad.Reader (ReaderT, ask, runReaderT)
-import Data.Argonaut (class EncodeJson, encodeJson)
-import Data.Array (replicate)
+import Control.Monad.Reader (ReaderT, asks, runReaderT)
+import Data.Argonaut (class EncodeJson)
+import Data.Argonaut.Encode.Generic.Rep (genericEncodeJson)
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Show (genericShow)
 import Data.Maybe (Maybe(Nothing))
+import Data.Array.NonEmpty (NonEmptyArray, cons', head, toArray)
 import Effect (Effect)
-import Effect.Aff (Aff)
 import Effect.Console (log)
 import Node.HTTP (createServer, listen)
 import Nodetrout.Error (HTTPError)
 import Nodetrout.Server (serve)
-import Text.Smolder.HTML (p)
+import Text.Smolder.HTML (p, span)
 import Text.Smolder.Markup (text)
 import Type.Proxy (Proxy(..))
-import Type.Trout (type (:=), type (:>), type (:<|>), Lit, Resource)
+import Type.Trout (type (:=), type (:/), type (:>), type (:<|>), Lit, Resource)
 import Type.Trout.ContentType.HTML (class EncodeHTML, HTML)
 import Type.Trout.ContentType.JSON (JSON)
 import Type.Trout.Method (Get)
 
-data Greeting = Greeting String
+newtype Greeting = Greeting { id :: Int, message :: String }
 
-type Site = "greeting" := Lit "greeting" :> Resource (Get Greeting (JSON :<|> HTML))
-       :<|> "greetings" := Lit "greetings" :> Resource (Get (Array Greeting) JSON)
+derive instance genericGreeting :: Generic Greeting _
+
+instance showGreeting :: Show Greeting where
+  show = genericShow
 
 instance encodeJsonGreeting :: EncodeJson Greeting where
-  encodeJson (Greeting g) = encodeJson g
+  encodeJson = genericEncodeJson
 
 instance encodeHTMLGreeting :: EncodeHTML Greeting where
-  encodeHTML (Greeting g) = p (text g)
+  encodeHTML = show >>> text >>> span
 
-runAppM :: forall a. String -> ReaderT String Aff a -> Aff a
-runAppM = flip runReaderT
+type Site = "greetings" := Lit "greetings" :> Resource (Get (Array Greeting) JSON)
+       :<|> "greeting" := Lit "greeting" :> Resource (Get Greeting (JSON :<|> HTML))
 
 site :: Proxy Site
 site = Proxy
 
+greetings :: NonEmptyArray Greeting
+greetings = map Greeting $ cons' { id: 1, message: "Hi" }
+                               [ { id: 2, message: "Hello" }
+                               , { id: 3, message: "Hey" }
+                               ]
+
 resources
   :: forall m
    . Monad m
-  => { greeting :: { "GET" :: ExceptT HTTPError (ReaderT String m) Greeting }
-     , greetings :: { "GET" :: ExceptT HTTPError (ReaderT String m) (Array Greeting) }
+  => { greeting :: { "GET" :: ExceptT HTTPError (ReaderT (NonEmptyArray Greeting) m) Greeting }
+     , greetings :: { "GET" :: ExceptT HTTPError (ReaderT (NonEmptyArray Greeting) m) (Array Greeting) }
      }
 resources =
-  { greeting: { "GET": Greeting <$> ask }
-  , greetings: { "GET": replicate 5 <<< Greeting <$> ask }
+  { greeting: { "GET": asks head }
+  , greetings: { "GET": asks toArray }
   }
 
 main :: Effect Unit
 main = do
-  server <- createServer $ serve site resources (runAppM "Hello")
+  server <- createServer $ serve site resources (flip runReaderT greetings)
   listen server { hostname: "0.0.0.0", port: 3000, backlog: Nothing } $ log "Listening on port 3000..."
