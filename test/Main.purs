91f0a78f0f1b789bd25be8ac3e6fa5bae6c3f3e8
Nick Saunders <nick@saunde.rs>
2019-10-08 08:03:32 -0700

    Get test site and first test working.

--- test/Main.purs
+++ test/Main.purs
@@ -1,11 +1,53 @@
 module Test.Main where
 
 import Prelude
-
+import Control.Monad.Except (runExceptT)
+import Data.Either (Either(..))
+import Data.Lazy (Lazy, defer)
+import Data.Maybe (Maybe(..))
+import Data.MediaType (MediaType)
+import Data.MediaType.Common (textHTML)
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Aff (Aff, launchAff_)
+import Effect.Aff.Class (class MonadAff)
+import Foreign.Object (Object)
+import Foreign.Object (empty, insert) as FO
+import Nodetrout.Error (HTTPError)
+import Nodetrout.Request (Request(..))
+import Nodetrout.Router (route)
+import Test.Spec (describe, it)
+import Test.Spec.Assertions (fail, shouldEqual)
+import Test.Spec.Reporter (consoleReporter)
+import Test.Spec.Runner (runSpec)
+import TestSite (resources, site)
+
+type RequestSpec =
+  { method :: String
+  , url :: String
+  , headers :: Object String
+  , readString :: Lazy (Aff (Maybe String))
+  } 
+
+defaultRequest :: RequestSpec
+defaultRequest =
+  { method: "GET"
+  , url: "/"
+  , headers: FO.empty
+  , readString: defer $ const (pure Nothing)
+  }
+
+processRequest :: forall m. Monad m => MonadAff m => RequestSpec -> m (Either HTTPError (Tuple MediaType String))
+processRequest = runExceptT <<< route site resources <<< Request
 
 main :: Effect Unit
-main = do
-  log "🍝"
-  log "You should add some tests."
+main = launchAff_ $ runSpec [consoleReporter] do
+  describe "content negotiation" do
+    it "should provide the content in the client's preferred format when available" do
+      result <- processRequest $ defaultRequest { headers = FO.insert "accept" "text/html" defaultRequest.headers }
+      case result of
+        Left _ ->
+          fail "Request failed unexpectedly."
+        Right (Tuple mediaType content) -> do
+          mediaType `shouldEqual` textHTML
+          content `shouldEqual` "<h1>Home Page</h1>"
--- test/TestSite.purs
+++ test/TestSite.purs
@@ -1,11 +1,17 @@
 module TestSite where
 
 import Prelude
-import Control.Monad.Except (ExceptT)
+import Control.Monad.Except (ExceptT, throwError)
 import Data.Argonaut (class DecodeJson, class EncodeJson, jsonEmptyObject)
-import Data.Newtype (class Newtype)
-import Nodetrout.Error (HTTPError)
-import Nodetrout.Router (route)
+import Data.Array (filter)
+import Data.Either (Either(Left))
+import Data.Foldable (find, foldr)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Newtype (class Newtype, un)
+import Data.String (contains, toLower) as String
+import Data.String.Pattern (Pattern(..))
+import Network.HTTP (status404)
+import Nodetrout.Error (HTTPError(..))
 import Text.Smolder.HTML (h1)
 import Text.Smolder.Markup (text)
 import Type.Proxy (Proxy(..))
@@ -24,7 +30,7 @@ import Type.Trout
 import Type.Trout.ContentType.HTML (HTML, class EncodeHTML)
 import Type.Trout.ContentType.JSON (JSON)
 import Type.Trout.Method (Get, Post)
-import Type.Trout.PathPiece (class FromPathPiece, class ToPathPiece)
+import Type.Trout.PathPiece (class FromPathPiece)
 
 data Default = Default
 
@@ -42,10 +48,23 @@ derive newtype instance decodeJsonMessage :: DecodeJson Message
 
 derive newtype instance encodeJsonMessage :: EncodeJson Message
 
+newtype PathBoolean = PathBoolean Boolean
+
+instance fromPathPiecePathBoolean :: FromPathPiece PathBoolean where
+  fromPathPiece = map PathBoolean <<< case _ of
+    "false" -> pure false
+    "0" -> pure false
+    "true" -> pure true
+    "1" -> pure true
+    unrecognized -> Left unrecognized
+
+
+derive instance newtypePathBoolean :: Newtype PathBoolean _
+
 type Site = "default" := Resource (Get Default (JSON :<|> HTML))
-       :<|> "messages" := "api" :/ "messages" :/ QueryParams "content" :/ QueryParam "unread" :/ Resource (Get (Array Message) JSON)
-       :<|> "messageById" := "api" :/ "messages" :/ Capture "id" Int :/ Resource (Get Message JSON)
-       :<|> "messagesById" := "api" :/ "messages" :/ CaptureAll "id" Int :/ Resource (Get (Array Message) JSON)
+       :<|> "messages" := "api" :/ "messages" :/ QueryParams "content" String :> QueryParam "unread" PathBoolean :> Resource (Get (Array Message) JSON)
+       :<|> "messageById" := "api" :/ "messages" :/ Capture "id" Int :> Resource (Get Message JSON)
+       :<|> "messagesById" := "api" :/ "messages" :/ CaptureAll "id" Int :> Resource (Get (Array Message) JSON)
        :<|> "newMessage" := "api" :/ "messages" :/ ReqBody Message JSON :> Resource (Post Message JSON)
 
 site :: Proxy Site
@@ -54,13 +73,16 @@ site = Proxy
 type Handler m = ExceptT HTTPError m
 
 messages :: Array Message
-messages = Messages <$> [ { id: 1, content: "Hello", unread: true }
-                        , { id: 2, content: "Hi", unread: true }
-                        , { id: 3, content: "How are you today?", unread: false }
-                        ]
+messages = Message <$> [ { id: 1, content: "Hello", unread: true }
+                       , { id: 2, content: "Hi", unread: true }
+                       , { id: 3, content: "How are you today?", unread: false }
+                       ]
 
 messageHasContent :: String -> Message -> Boolean
-messageHasContent c (Message { content }) = String.contains (String.toLower content) (Pattern $ String.toLower c)
+messageHasContent c (Message { content }) = String.contains (Pattern $ String.toLower c) (String.toLower content)
+
+messageMatchesId :: Int -> Message -> Boolean
+messageMatchesId expected (Message { id }) = id == expected
 
 messageUnread :: Message -> Boolean
 messageUnread (Message { unread }) = unread
@@ -69,13 +91,23 @@ resources
   :: forall m
    . Monad m
   => { default :: { "GET" :: Handler m Default }
-     , messages :: Array String -> Boolean -> { "GET" :: Handler m (Array Message) }
+     , messages :: Array String -> Maybe PathBoolean -> { "GET" :: Handler m (Array Message) }
      , messageById :: Int -> { "GET" :: Handler m Message }
      , messagesById :: Array Int -> { "GET" :: Handler m (Array Message) }
      , newMessage :: Message -> { "POST" :: Handler m Message }
      }
 resources =
   { default: { "GET": pure Default }
-  , messages: \con unr -> { "GET": pure $ filter (\m -> messageHasContent con m || messageUnread m == unr) messages }
-  , addMessage: \message -> { "POST": pure message }
+  , messages: \c -> map (un PathBoolean) >>> \u ->
+      { "GET": pure $ filter (foldr (||) (eq (fromMaybe false u) <<< messageUnread) (messageHasContent <$> c)) messages }
+  , messageById: \id ->
+      { "GET":
+          case find (messageMatchesId id) messages of
+            Just message ->
+              pure message
+            Nothing ->
+              throwError $ HTTPError { status: status404, details: Just $ "No message has ID " <> show id <> "." }
+      }
+  , messagesById: \ids -> { "GET": pure $ filter (foldr (||) (const false) (messageMatchesId <$> ids)) messages }
+  , newMessage: \message -> { "POST": pure message }
   }
