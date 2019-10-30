43ee7ebb7cc2d2712765f5830ca0c1dce0ab4621
Nick Saunders <nick@saunde.rs>
2019-10-30 06:19:20 -0700

    Don't crash the server when an unhandled error is thrown.

--- src/Nodetrout/Server/Node.purs
+++ src/Nodetrout/Server/Node.purs
@@ -10,8 +10,9 @@ import Data.Maybe (Maybe(..), fromMaybe)
 import Data.MediaType (MediaType(..))
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
-import Effect.Aff (Aff, launchAff_, makeAff, nonCanceler)
+import Effect.Aff (Aff, makeAff, nonCanceler, runAff_)
 import Effect.Class (class MonadEffect, liftEffect)
+import Effect.Exception (Error)
 import Effect.Ref (modify_, new, read) as Ref
 import Node.Buffer (concat, toString) as Buffer
 import Node.Encoding (Encoding(UTF8))
@@ -58,24 +59,37 @@ serve
   => Proxy layout
   -> Record handlers
   -> (m ~> Aff)
+  -> (Error -> Effect Unit)
   -> NH.Request
   -> NH.Response
   -> Effect Unit
-serve layout handlers runM req res = launchAff_ $ runM do
-  let rs = responseAsStream res
-  result <- runExceptT $ route layout handlers (convertRequest req)
-  liftEffect $ case result of
-    Left error -> do
-      setStatusCode res $ error ^. _errorStatusCode
-      setHeader res "content-type" "text/plain"
-      let body = error ^. _errorOverview <> fromMaybe "" ((\d -> ": " <> d) <$> error ^. _errorDetails)
-      _ <- Stream.writeString rs UTF8 body $ pure unit
-      Stream.end rs $ pure unit
-    Right (Tuple (MediaType contentType) content) -> do
-      setStatusCode res 200
-      setHeader res "content-type" contentType
-      writeResponse rs content
-      Stream.end rs $ pure unit
+serve layout handlers runM onError req res =
+  let
+    rs = responseAsStream res
+    requestCallback = case _ of
+      Right _ ->
+        pure unit
+      Left error -> do
+        setStatusCode res 500
+        setHeader res "content-type" "text/plain"
+        _ <- Stream.writeString rs UTF8 "An unexpected error occurred while processing the request." $ pure unit
+        Stream.end rs $ pure unit
+        onError error
+  in
+    runAff_ requestCallback $ runM do
+      result <- runExceptT $ route layout handlers (convertRequest req)
+      liftEffect $ case result of
+        Left error -> do
+          setStatusCode res $ error ^. _errorStatusCode
+          setHeader res "content-type" "text/plain"
+          let body = error ^. _errorOverview <> fromMaybe "" ((\d -> ": " <> d) <$> error ^. _errorDetails)
+          _ <- Stream.writeString rs UTF8 body $ pure unit
+          Stream.end rs $ pure unit
+        Right (Tuple (MediaType contentType) content) -> do
+          setStatusCode res 200
+          setHeader res "content-type" contentType
+          writeResponse rs content
+          Stream.end rs $ pure unit
 
 class ResponseWritable content where
   writeResponse :: Stream.Writable () -> content -> Effect Unit
