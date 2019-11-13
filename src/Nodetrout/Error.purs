a10e2464736239347d4299ca3ca6a8dca534aeac
Nick Saunders <nick@saunde.rs>
2019-11-12 21:36:46 -0700

    Prioritize errors based on route specificity. (#4)

--- src/Nodetrout/Error.purs
+++ src/Nodetrout/Error.purs
@@ -1,7 +1,6 @@
 module Nodetrout.Error where
   
 import Prelude
-import Data.Foldable (elem)
 import Data.Lens (Lens', (^.), lens)
 import Data.Maybe (Maybe(..))
 
@@ -9,6 +8,7 @@ newtype HTTPError = HTTPError
   { statusCode :: Int
   , overview :: String
   , details :: Maybe String
+  , priority :: Int
   }
 
 instance showHTTPError :: Show HTTPError where
@@ -17,116 +17,135 @@ instance showHTTPError :: Show HTTPError where
 _errorStatusCode :: Lens' HTTPError Int
 _errorStatusCode = lens
   (\(HTTPError { statusCode }) -> statusCode)
-  (\(HTTPError e) statusCode -> HTTPError $ e { statusCode = statusCode })
+  (\(HTTPError e) statusCode -> HTTPError e { statusCode = statusCode })
 
 _errorOverview :: Lens' HTTPError String
 _errorOverview = lens
   (\(HTTPError { overview }) -> overview)
-  (\(HTTPError e) overview -> HTTPError $ e { overview = overview })
+  (\(HTTPError e) overview -> HTTPError e { overview = overview })
 
 _errorDetails :: Lens' HTTPError (Maybe String)
 _errorDetails = lens
   (\(HTTPError { details }) -> details)
-  (\(HTTPError e) details -> HTTPError $ e { details = details })
- 
+  (\(HTTPError e) details -> HTTPError e { details = details })
+
+_errorPriority :: Lens' HTTPError Int
+_errorPriority = lens
+  (\(HTTPError { priority }) -> priority)
+  (\(HTTPError e) priority -> HTTPError e { priority = priority })
+
 select :: HTTPError -> HTTPError -> HTTPError
 select error1 error2
-  | (error1 ^. _errorStatusCode) `elem` [404, 405] && error2 ^. _errorStatusCode == 404 = error1
-  | error1 ^. _errorStatusCode /= 404 && (error2 ^. _errorStatusCode) `elem` [404, 405] = error1
-  | otherwise = error2
+  | error1 ^. _errorPriority > error2 ^. _errorPriority = error1
+  | error1 ^. _errorPriority < error2 ^. _errorPriority = error2
+  | statusCodePriority error1 < statusCodePriority error2 = error2
+  | otherwise = error1
+
+statusCodePriority :: HTTPError -> Int
+statusCodePriority error = case error ^. _errorStatusCode of
+  404 -> 0
+  405 -> 1
+  401 -> 2
+  415 -> 3
+  406 -> 4
+  400 -> 6
+  _ -> 5
+
+defaultError :: Int -> String -> HTTPError
+defaultError statusCode overview = HTTPError { statusCode, overview, details: Nothing, priority: 1000000 }
 
 error300 :: HTTPError
-error300 = HTTPError { statusCode: 300, overview: "Multiple Choices", details: Nothing }
+error300 = defaultError 300 "Multiple Choices"
 
 error301 :: HTTPError
-error301 = HTTPError { statusCode: 301, overview: "Moved Permanently", details: Nothing }
+error301 = defaultError 301 "Moved Permanently"
 
 error302 :: HTTPError
-error302 = HTTPError { statusCode: 302, overview: "Found", details: Nothing }
+error302 = defaultError 302 "Found"
 
 error303 :: HTTPError
-error303 = HTTPError { statusCode: 303, overview: "See Other", details: Nothing }
+error303 = defaultError 303 "See Other"
 
 error304 :: HTTPError
-error304 = HTTPError { statusCode: 304, overview: "Not Modified", details: Nothing }
+error304 = defaultError 304 "Not Modified"
 
 error305 :: HTTPError
-error305 = HTTPError { statusCode: 305, overview: "Use Proxy", details: Nothing }
+error305 = defaultError 305 "Use Proxy"
 
 error307 :: HTTPError
-error307 = HTTPError { statusCode: 307, overview: "Temporary Redirect", details: Nothing }
+error307 = defaultError 307 "Temporary Redirect"
 
 error400 :: HTTPError
-error400 = HTTPError { statusCode: 400, overview: "Bad Request", details: Nothing }
+error400 = defaultError 400 "Bad Request"
 
 error401 :: HTTPError
-error401 = HTTPError { statusCode: 401, overview: "Unauthorized", details: Nothing }
+error401 = defaultError 401 "Unauthorized"
 
 error402 :: HTTPError
-error402 = HTTPError { statusCode: 402, overview: "Payment Required", details: Nothing }
+error402 = defaultError 402 "Payment Required"
 
 error403 :: HTTPError
-error403 = HTTPError { statusCode: 403, overview: "Forbidden", details: Nothing }
+error403 = defaultError 403 "Forbidden"
 
 error404 :: HTTPError
-error404 = HTTPError { statusCode: 404, overview: "Not Found", details: Nothing }
+error404 = defaultError 404 "Not Found"
 
 error405 :: HTTPError
-error405 = HTTPError { statusCode: 405, overview: "Method Not Allowed", details: Nothing }
+error405 = defaultError 405 "Method Not Allowed"
 
 error406 :: HTTPError
-error406 = HTTPError { statusCode: 406, overview: "Not Acceptable", details: Nothing }
+error406 = defaultError 406 "Not Acceptable"
 
 error407 :: HTTPError
-error407 = HTTPError { statusCode: 407, overview: "Proxy Authentication Required", details: Nothing }
+error407 = defaultError 407 "Proxy Authentication Required"
 
 error409 :: HTTPError
-error409 = HTTPError { statusCode: 409, overview: "Conflict", details: Nothing }
+error409 = defaultError 409 "Conflict"
 
 error410 :: HTTPError
-error410 = HTTPError { statusCode: 410, overview: "Gone", details: Nothing }
+error410 = defaultError 410 "Gone"
 
 error411 :: HTTPError
-error411 = HTTPError { statusCode: 411, overview: "Length Required", details: Nothing }
+error411 = defaultError 411 "Length Required"
 
 error412 :: HTTPError
-error412 = HTTPError { statusCode: 412, overview: "Precondition Failed", details: Nothing }
+error412 = defaultError 412 "Precondition Failed"
 
 error413 :: HTTPError
-error413 = HTTPError { statusCode: 413, overview: "Request Entity Too Large", details: Nothing }
+error413 = defaultError 413 "Request Entity Too Large"
 
 error414 :: HTTPError
-error414 = HTTPError { statusCode: 414, overview: "Request-URI Too Large", details: Nothing }
+error414 = defaultError 414 "Request-URI Too Large"
 
 error415 :: HTTPError
-error415 = HTTPError { statusCode: 415, overview: "Unsupported Media Type", details: Nothing }
+error415 = defaultError 415 "Unsupported Media Type"
 
 error416 :: HTTPError
-error416 = HTTPError { statusCode: 416, overview: "Request range not satisfiable", details: Nothing }
+error416 = defaultError 416 "Request range not satisfiable"
 
 error417 :: HTTPError
-error417 = HTTPError { statusCode: 417, overview: "Expectation Failed", details: Nothing }
+error417 = defaultError 417 "Expectation Failed"
 
 error418 :: HTTPError
-error418 = HTTPError { statusCode: 418, overview: "I'm a teapot", details: Nothing }
+error418 = defaultError 418 "I'm a teapot"
 
 error422 :: HTTPError
-error422 = HTTPError { statusCode: 422, overview: "Unprocessable Entity", details: Nothing }
+error422 = defaultError 422 "Unprocessable Entity"
 
 error500 :: HTTPError
-error500 = HTTPError { statusCode: 500, overview: "Internal Server Error", details: Nothing }
+error500 = defaultError 500 "Internal Server Error"
 
 error501 :: HTTPError
-error501 = HTTPError { statusCode: 501, overview: "Not Implemented", details: Nothing }
+error501 = defaultError 501 "Not Implemented"
 
 error502 :: HTTPError
-error502 = HTTPError { statusCode: 502, overview: "Bad Gateway", details: Nothing }
+error502 = defaultError 502 "Bad Gateway"
 
 error503 :: HTTPError
-error503 = HTTPError { statusCode: 503, overview: "Service Unavailable", details: Nothing }
+error503 = defaultError 503 "Service Unavailable"
 
 error504 :: HTTPError
-error504 = HTTPError { statusCode: 504, overview: "Gateway Time-out", details: Nothing }
+error504 = defaultError 504 "Gateway Time-out"
 
 error505 :: HTTPError
-error505 = HTTPError { statusCode: 505, overview: "HTTP Version not supported", details: Nothing }
+error505 = defaultError 505 "HTTP Version not supported"
