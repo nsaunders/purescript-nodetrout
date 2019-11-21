b39d271507f222e74483a30661ad84a0f0dd4c85
Nick Saunders <nick@saunde.rs>
2019-11-21 12:41:05 -0700

    Document Request module.

--- src/Nodetrout/Internal/Request.purs
+++ src/Nodetrout/Internal/Request.purs
@@ -17,6 +17,8 @@ import Data.Tuple (Tuple(..), fst, snd)
 import Effect.Aff (Aff)
 import Foreign.Object (Object, toArrayWithKey)
 
+-- | A `Request` contains the request method, URL, and headers, as well as an
+-- | `Aff` that reads the request body, which are used in the routing logic.
 newtype Request = Request
   { method :: String
   , url :: String
@@ -26,22 +28,28 @@ newtype Request = Request
 
 derive instance newtypeRequest :: Newtype Request _
 
+-- | Gets and parses the request method.
 method :: Request -> Either Method CustomMethod
 method = Method.fromString <<< _.method <<< un Request
 
+-- | Gets and parses the request path (URL).
 path :: Request -> Array String
 path = filter (_ /= "") <<< String.split (Pattern "/") <<< String.takeWhile (_ /= '?') <<< _.url <<< un Request
 
+-- | Gets and parses the request query string parameters.
 query :: Request -> Array (Tuple String (Maybe String))
 query (Request { url }) =
   fromMaybe [] $ un FormURLEncoded <$> FUE.decode (String.drop 1 $ String.dropWhile (_ /= '?') url)
 
+-- | Gets the request headers.
 headers :: Request -> Object String
 headers = _.headers <<< un Request
 
+-- | Gets the request body as a string.
 stringBody :: Request -> Aff (Maybe String)
 stringBody = _.stringBody <<< un Request
 
+-- | Replaces the request path. Useful for scoping when routing the request.
 replacePath :: Array String -> Request -> Request
 replacePath p (Request r) =
   let
@@ -49,6 +57,8 @@ replacePath p (Request r) =
   in
     Request $ r { url = "/" <> String.joinWith "/" p <> "?" <> queryString }
 
+-- | Gets the next segment from the request path if available and returns it
+-- | along with a scoped request. Useful when routing the request.
 unconsPath :: Request -> Tuple (Maybe String) Request
 unconsPath request = case uncons $ path request of
   Just { head, tail } ->
@@ -56,15 +66,24 @@ unconsPath request = case uncons $ path request of
   Nothing ->
     Tuple Nothing request
 
+-- | Captures the remaining segments from the request path and returns them along
+-- | with a scoped request. Useful when routing the request.
 removePath :: Request -> Tuple (Array String) Request
 removePath request = Tuple (path request) $ replacePath [] request
 
+-- | Attempts to get the value from a request header.
 headerValue :: String -> Request -> Maybe String
 headerValue name =
   map snd <<< find ((_ == String.toLower name) <<< String.toLower <<< fst) <<< toArrayWithKey Tuple <<< headers
 
+-- | Gets all values from the query string matching the specified label (key).
+-- | For example, given a label of `foo` and a request with a query string of
+-- | `?foo=bar&foo=baz`, this would return an array `["bar", "baz"]`.
 queryParamValues :: String -> Request -> Array String
 queryParamValues label = catMaybes <<< map snd <<< filter (eq label <<< fst) <<< query
 
+-- | Gets the first value from the query string matching the specified label
+-- | (key). For example, given a label of `foo` and a request with a query string
+-- | of `?foo=bar&foo=baz`, this would return a string `"bar"`.
 queryParamValue :: String -> Request -> Maybe String
 queryParamValue label = head <<< queryParamValues label
