70e1b09015f8765ee6c2295dbe27b306bbda4f79
Nick Saunders <nick@saunde.rs>
2019-11-25 06:20:28 -0700

    Improve Header example.

--- example/Header.purs
+++ example/Header.purs
@@ -1,9 +1,13 @@
 module Example.Header where
 
 import Prelude
-import Control.Monad.Except (ExceptT, except, throwError)
+import Control.Monad.Except (ExceptT)
+import Data.Argonaut (class EncodeJson, encodeJson)
 import Data.Bifunctor (lmap)
+import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
+import Data.Newtype (class Newtype, un)
+import Data.Tuple (Tuple(..), fst)
 import Data.String.Base64 (decode)
 import Data.String.CodePoints (drop, take) as String
 import Data.String.Common (split, toLower) as String
@@ -12,36 +16,42 @@ import Effect (Effect)
 import Effect.Console (log)
 import Effect.Exception (message)
 import Node.HTTP (createServer, listen)
-import Nodetrout (HTTPError, error400, serve')
+import Nodetrout (HTTPError, serve')
 import Type.Proxy (Proxy(..))
-import Type.Trout (type (:=), type (:>), Header, Resource)
+import Type.Trout (type (:=), type (:/), type (:>), Header, Resource)
 import Type.Trout.ContentType.JSON (JSON)
+import Type.Trout.Header (class FromHeader)
 import Type.Trout.Method (Get)
 
-type Credentials = { username :: String, password :: String }
+newtype BasicAuth = BasicAuth (Tuple String String)
 
-type Site = "credentials" := Header "Authorization" String :> Resource (Get Credentials JSON)
+derive instance newtypeBasicAuth :: Newtype BasicAuth _
+
+instance fromHeaderBasicAuth :: FromHeader BasicAuth where
+  fromHeader headerValue
+    | String.toLower (String.take 6 headerValue) /= "basic " = Left "Only Basic authorization is supported."
+    | otherwise = do
+        payload <- lmap (\e -> "Failed to decode header: " <> message e) $ decode (String.drop 6 headerValue)
+        case String.split (Pattern ":") payload of
+          [ user, pass ] ->
+            pure $ BasicAuth $ Tuple user pass
+          _ ->
+            Left "The Authorization header is invalid."
+
+newtype Greeting = Greeting String
+
+derive instance newtypeGreeting :: Newtype Greeting _
+
+instance encodeJsonGreeting :: EncodeJson Greeting where
+  encodeJson = encodeJson <<< un Greeting
+
+type Site = "admin" := "admin" :/ Header "Authorization" BasicAuth :> Resource (Get Greeting JSON)
 
 site :: Proxy Site
 site = Proxy
 
-resources :: forall m. Monad m => { credentials :: String -> { "GET" :: ExceptT HTTPError m Credentials } }
-resources =
-  { credentials:
-      \auth ->
-        { "GET": do
-            when ((String.toLower $ String.take 6 auth) /= "basic ")
-              $ throwError error400 { details = Just "Only Basic authorization is supported." }
-            authStr <- except
-                       $ lmap (\e -> error400 { details = Just ("Failed to decode header: " <> message e) })
-                       $ decode $ String.drop 6 auth
-            case String.split (Pattern ":") authStr of
-              [ username, password ] ->
-                pure { username, password }
-              _ ->
-                throwError error400 { details = Just "The Authorization header is invalid." }
-        }
-    }
+resources :: forall m. Monad m => { admin :: BasicAuth -> { "GET" :: ExceptT HTTPError m Greeting } }
+resources = { admin: \auth -> { "GET": pure $ Greeting $ "Hello, " <> (fst $ un BasicAuth auth) } }
 
 main :: Effect Unit
 main = do
