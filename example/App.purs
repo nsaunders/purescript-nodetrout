ffb1532dceb06e6ecba3a9490777c6745583f2f4
Nick Saunders <nick@saunde.rs>
2020-01-28 06:51:19 -0700

    Enhance "middleware" example.

--- example/App.purs
+++ example/App.purs
@@ -4,13 +4,18 @@ import Prelude
 import Control.Monad.Except (ExceptT, throwError)
 import Control.Monad.Reader (ReaderT, runReaderT)
 import Control.Monad.Reader.Class (class MonadAsk, class MonadReader, ask, local)
+import Control.Monad.Writer (WriterT, runWriterT)
+import Control.Monad.Writer.Class (class MonadTell, class MonadWriter, tell)
 import Data.Maybe (Maybe(..))
 import Data.Newtype (class Newtype, un)
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Aff (Aff)
-import Effect.Class (class MonadEffect)
-import Effect.Console (log)
-import Node.HTTP (createServer, listen)
+import Effect.Class (class MonadEffect, liftEffect)
+import Effect.Console (log, logShow)
+import Foreign.Object (lookup) as FO
+import Node.HTTP (createServer, listen, requestHeaders)
+import Node.HTTP (Request) as NH
 import Nodetrout (HTTPError, error402)
 import Nodetrout.Internal.Server.Node (serve)
 import Text.Smolder.HTML (span)
@@ -27,7 +32,7 @@ derive instance newtypeMessage :: Newtype Message _
 instance encodeHTMLMessage :: EncodeHTML Message where
   encodeHTML = un Message >>> text >>> span
 
-newtype AppM a = AppM (ReaderT Message Aff a)
+newtype AppM a = AppM (ReaderT Message (WriterT (Array String) Aff) a)
 
 derive instance newtypeAppM :: Newtype (AppM a) _
 
@@ -39,9 +44,14 @@ derive newtype instance monadAppM :: Monad AppM
 derive newtype instance monadEffectAppM :: MonadEffect AppM
 derive newtype instance monadAskMessageAppM :: MonadAsk Message AppM
 derive newtype instance monadReaderMessageAppM :: MonadReader Message AppM
+derive newtype instance monadTellLogAppM :: MonadTell (Array String) AppM
+derive newtype instance monadWriterLogAppM :: MonadWriter (Array String) AppM
 
-runAppM :: forall a. Message -> AppM a -> Aff a
-runAppM message = flip runReaderT message <<< un AppM
+runAppM :: AppM ~> Aff
+runAppM (AppM appM) = do
+  Tuple x logMessages <- runWriterT (runReaderT appM $ Message "Hello, Sailor!")
+  liftEffect $ logShow logMessages
+  pure x
 
 type Site = "message" := Resource (Get Message HTML)
 
@@ -51,12 +61,28 @@ site = Proxy
 resources :: { message :: { "GET" :: ExceptT HTTPError AppM Message } }
 resources = { message: { "GET": ask } }
 
-shakedown :: forall r m a. Monad m => MonadReader Message m => r -> ExceptT HTTPError m a -> ExceptT HTTPError m a
-shakedown _ x = local (const $ Message "Temp Message") x
---shakedown :: forall r m a. Monad m => r -> ExceptT HTTPError m a -> ExceptT HTTPError m a
---shakedown _ _ = throwError error402 { details = Just "You have to pay me first." }
+messageFromHeader
+  :: forall m a
+   . Monad m
+  => MonadReader Message m
+  => MonadTell (Array String) m
+  => NH.Request
+  -> ExceptT HTTPError m a
+  -> ExceptT HTTPError m a
+messageFromHeader req x =
+  case FO.lookup "x-message" $ requestHeaders req of
+    Nothing -> do
+      tell ["Handling request as normal..."]
+      foo <- x
+      tell ["Finished handling request as normal."]
+      pure foo
+    Just message -> do
+      tell ["Handling request with alternate message..."]
+      foo <- local (const $ Message message) x
+      tell ["Finished handling request with alternate message."]
+      pure foo
 
 main :: Effect Unit
 main = do
-  server <- createServer $ serve site resources (runAppM $ Message "Hello, Sailor!") shakedown (const $ pure unit)
+  server <- createServer $ serve site resources runAppM messageFromHeader (const $ pure unit)
   listen server { hostname: "0.0.0.0", port: 3000, backlog: Nothing } $ log "Listening on port 3000..."
